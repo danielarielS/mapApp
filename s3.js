e097b1f5e6b0b0815da6df8d7061e7b15aa215a0
daniel ariel <danielariel70@gmail.com>
2018-05-27 13:24:33 +0200

    aws

--- s3.js
+++ s3.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV == "production") {
 const client = knox.createClient({
     key: secrets.AWS_KEY,
     secret: secrets.AWS_SECRET,
-    bucket: "spicyturkeyangie"
+    bucket: "danielspiced"
 });
 
 exports.upload = function(req, res, next) {
